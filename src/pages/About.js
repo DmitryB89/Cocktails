a2c9e53e586b4afa52374d1fe5f8b98ab020ab86
Dmitry <d.baydarov@yandex.ru>
2023-02-19 20:12:46 +0300

    cocktails setup

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className={'section about-section'}>
+            <h1 className={'section-title'}></h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum mollitia nobis possimus
+                quiatempore vitae. Autem corporis dolore dolores est illum ipsam, iusto maxime, quidem repellat
+                repellendus sit, sunt ut. Deleniti doloribus eum obcaecati soluta ut. Incidunt, natus, non. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Blanditiis eum natus nemo tempora. Ab culpa cum deserunt excepturi illum saepe soluta. Illum incidunt iure magnam pariatur praesentium!
+                Alias explicabo minima neque? A ad adipisci doloribus libero pariatur.
+                Asperiores consectetur consequatur consequuntur earum exercitationem iste libero quibusdam reiciendis rem, suscipit, veniam!</p>
+        </section>
+    )
+}
+
+export default About
